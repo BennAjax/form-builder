1e1f29417c6d5bb2b867a203c1fca8fb60c007d8
BennAjax <bennkeys1@gmail.com>
2022-10-05 00:30:15 +0100

    frontend: add basic views and components

--- frontend/src/views/CreateAccount.jsx
+++ frontend/src/views/CreateAccount.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const CreateAccount = () => {
   return (
     <div className='grid grid-cols-2 w-screen h-screen'>
       <div className='bg-indigo-600 col-span-1'></div>
       <div className='col-span-1 flex items-center justify-center'>
         <form className='flex flex-col items-center space-y-6'>
           <div className='flex flex-col items-center justify-center space-y-4'>
-            <h1 className='text-3xl font-bold smm:text-4xl'>Hello again!</h1>
-            <p>Login now to access you forms.</p>
+            <h1 className='text-3xl font-bold smm:text-4xl'>
+              Welcome to Form Builder!
+            </h1>
+            <p>
+              The fastest way to build your online forms. Let's get you started
+            </p>
+          </div>
+          <div className='flex flex-col space-y-2'>
+            <input
+              type='text'
+              placeholder='Your name'
+              name='fullname'
+              id='fullname'
+              className='p-4 bg-indigo-50 rounded-md min-w-[24rem] outline-indigo-400'
+            />
           </div>
           <div className='flex flex-col space-y-2'>
             <input
@@ -30,14 +43,14 @@ const Login = () => {
             />
           </div>
           <div className='flex flex-col space-y-2'>
-            <button className='p-4 bg-indigo-600 text-white font-medium rounded-md min-w-[24rem] outline-indigo-400'>
-              Login
+            <button className='p-4 bg-indigo-600 text-white font-medium rounded-md min-w-[24rem]'>
+              Create Account
             </button>
           </div>
           <div className='flex flex-col space-y-2 items-center'>
-            <p>Don't have an account?</p>
-            <Link to='/create-account' className='text-indigo-600 font-medium'>
-              Create an account
+            <p>Already have an account?</p>
+            <Link to='/' className='text-indigo-600 font-medium'>
+              Log-in instead
             </Link>
           </div>
         </form>
@@ -46,4 +59,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default CreateAccount;
